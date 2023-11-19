89120d7544cf37fa030b662ad1a6c3df0f5b3177
00seungho <tmdgh9910@naver.com>
2023-11-20 01:17:25 +0900

    gd

--- QuoteEngine/__init__.py
+++ QuoteEngine/__init__.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import List
 import os
+import csv
 class IngestorInterface(ABC):
     @classmethod
     @abstractmethod
@@ -11,14 +12,14 @@ class IngestorInterface(ABC):
     @abstractmethod
     def parse(cls, path: str) -> List[QuoteModel]:
         return []
-
+    
 class QuoteModel():
     def __init__(self, body, author):
         self.body = body
         self.author = author
     
 
-class Ingestor():
+class Ingestor(IngestorInterface):
     ingestors = []
     UseFileExtensions = [".csv",".docx",".pdf",".txt"]
     @classmethod
@@ -27,34 +28,36 @@ class Ingestor():
         for UseFileExtension in cls.UseFileExtensions:
             if UseFileExtension == FileExtension:
                 return True
-            return False
-    
+            return False    
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
-        cls.ingestors.append(QuoteModel())
         FileExtension = os.path.split(path)[1]
         if(Ingestor.can_ingest(path)):
             if FileExtension == cls.UseFileExtensions[0]:
-                return Ingestor.csv_parse
+                Ingestor.csv_parse(path)
             elif FileExtension == cls.UseFileExtensions[1]:
-                return Ingestor.docx_parse
+                Ingestor.docx_parse(path)
             elif FileExtension == cls.UseFileExtensions[2]:
-                return Ingestor.pdf_parse
+                Ingestor.pdf_parse(path)
             elif FileExtension == cls.UseFileExtensions[3]:
-                return Ingestor.txt_parse
-            
-
+                Ingestor.txt_parse(path)
+        return cls.ingestors
     @classmethod
-    def txt_parse():
+    def txt_parse(cls, path: str):
+        with open(path, "r")as file:
+            lines = file.readlines()
+            for item in lines:
+                body, author= item.split("-")
+                cls.ingestors.append(QuoteModel(body,author))
+    @classmethod
+    def csv_parse(cls, path: str):
+        with open(path, "r")as file:
+            lines = csv.reader(file)
+            for item in lines:
+                cls.ingestors.append(QuoteModel(item[0], item[1]))
+    @classmethod
+    def docx_parse(cls, path: str):
         
     @classmethod
-    def csv_parse():
-
-    @classmethod
-    def docx_parse():
-
-    @classmethod
-    def pdf_parse():
-
-
-            
+    def pdf_parse(cls,path: str):
+        
