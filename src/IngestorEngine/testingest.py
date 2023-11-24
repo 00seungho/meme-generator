4a65a5bc0eac0dfc87707c9004723944c29f6ea5
00seungho <tmdgh9910@naver.com>
2023-11-24 15:05:28 +0900

    hi

--- src/IngestorEngine/testingest.py
+++ src/IngestorEngine/testingest.py
@@ -3,5 +3,3 @@ a = TextIngestor()
 path = "D:\\2301110336\\udacity\\meme-generator-starter-code\\src\\_data\\SimpleLines\\SimpleLines.txt"
 
 b = a.parse(path)
-
-print("저자:"+b[1]._author+"내용:"+b[1]._body)
